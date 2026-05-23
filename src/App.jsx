bbd7cd7f735298b0653917c2523723ac8240f7ed
DirtX <ggdirtxgg@gmail.com>
2026-05-23 08:44:21 +0200

    perf: code-splitting + non-blocking fonts
    - Lazy-load project routes via React.lazy + Suspense
    - Load Google Fonts asynchronously via media swap trick
    - Add canonical, OG, Twitter Card, JSON-LD meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BackgroundGrid from "./components/BackgroundGrid";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import VideoCompressor from "./pages/Projects/VideoCompressor";
-import ColorPalette from "./pages/Projects/ColorPalette";
-import CSSToolkit from "./pages/Projects/CSSToolkit";
-import MarkdownEditor from "./pages/Projects/MarkdownEditor";
-import SVGEditor from "./pages/Projects/SVGEditor";
-import AudioExtractor from "./pages/Projects/AudioExtractor";
 import ScrollToTop from "./components/ScrollToTop";
 import SocialSidebar from "./components/SocialSidebar";
 import ContactModal from "./components/ContactModal";
 import { ModalProvider, useModal } from "./context/ModalContext";
 import "./App.css";
+
+// Feature: Lazy-load project routes to keep initial bundle small
+const Projects = lazy(() => import("./pages/Projects"));
+const VideoCompressor = lazy(() => import("./pages/Projects/VideoCompressor"));
+const ColorPalette = lazy(() => import("./pages/Projects/ColorPalette"));
+const CSSToolkit = lazy(() => import("./pages/Projects/CSSToolkit"));
+const MarkdownEditor = lazy(() => import("./pages/Projects/MarkdownEditor"));
+const SVGEditor = lazy(() => import("./pages/Projects/SVGEditor"));
+const AudioExtractor = lazy(() => import("./pages/Projects/AudioExtractor"));
 
 // Feature: Inner component that consumes modal context
 function AppContent() {
@@ -31,16 +34,18 @@ function AppContent() {
       <ContactModal open={contactModalOpen} onClose={closeContactModal} />
       {/* PAGE ROUTES */}
       <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/video-compressor" element={<VideoCompressor />} />
-          <Route path="/projects/color-palette" element={<ColorPalette />} />
-          <Route path="/projects/css-toolkit" element={<CSSToolkit />} />
-          <Route path="/projects/markdown-editor" element={<MarkdownEditor />} />
-          <Route path="/projects/svg-editor" element={<SVGEditor />} />
-          <Route path="/projects/audio-extractor" element={<AudioExtractor />} />
-        </Routes>
+        <Suspense fallback={<div />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/video-compressor" element={<VideoCompressor />} />
+            <Route path="/projects/color-palette" element={<ColorPalette />} />
+            <Route path="/projects/css-toolkit" element={<CSSToolkit />} />
+            <Route path="/projects/markdown-editor" element={<MarkdownEditor />} />
+            <Route path="/projects/svg-editor" element={<SVGEditor />} />
+            <Route path="/projects/audio-extractor" element={<AudioExtractor />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
